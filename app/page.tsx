8b8cfc16aa368f61bed6fef45df7f318fb551805
Alan Garber <alangarber1@gmail.com>
2023-03-24 12:56:33 -0300

    trying to show language to iso 693-1

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,33 @@
 'use client'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 const wash = require('washyourmouthoutwithsoap');
-
-
+import React from "react";
 
 export default function HomePage() {
-
     const languages = wash.supported();
-    console.log(languages);
-    const languageChosen = languages[Math.floor(Math.random() * languages.length)];
-    console.log(languageChosen);
-    
+    const [posLang,setPosLang]=useState()
+    const [languageChosen, setlanguageChosen] = useState('')
+    const [badWordLanguageChosen, setbadWordLanguageChosen] = useState()
+    const [arrBadWords, setArrBadWords] = useState([])
+    const [languagesTranslated, setlanguagesTranslated] = useState([
+        'belarusian', 'Bulgarian', 'Catalan', 'Czech', 'Welsh', 'Danish', 'German', 'Greek', 'English', 'Spanish', 'Estonian', 'Basque', 'Farsi', 'Finnish', 'French', 'Gaelic', 'Galician', 'Hindi', 'Croatian', 'Hungarian', 'Armenian', 'Indonesian', 'Icelandic', 'Italian', 'Japanese', 'Kannada', 'Korean', 'Latin', 'Lithuanian', 'Latvian', 'Macedonian', 'Malayalam', 'Mongolian', 'Marathi', 'Malay', 'Maltese', 'Burmese', 'Dutch', 'Polish', 'Portuguese', 'Romanian', 'Russian', 'Slovak', 'Slovenian', 'Albanian', 'Serbian', 'Swedish', 'Telugu', 'Thai', 'Turkish', 'Ukrainian', 'Uzbek', 'Vietnamese', 'Zulu'
+    ])
+
+
+    useEffect(() => {
+        setlanguageChosen(languages[Math.floor(Math.random() * languages.length)])
+    }, [])
+
+    useEffect(() => {
+        setArrBadWords(wash.words(languageChosen))
+        setPosLang(languages.indexOf(languageChosen))
+    }, [languageChosen])
+
+    useEffect(() => {
+        if (arrBadWords) {
+            setbadWordLanguageChosen(arrBadWords[Math.floor(Math.random() * arrBadWords.length)])
+        }
+    }, [arrBadWords])
 
     return (
         <html>
@@ -19,7 +36,9 @@ export default function HomePage() {
                     <h1>Que Rayos</h1>
                 </div>
                 <div>
+                    <h3>{badWordLanguageChosen}</h3>
                     <h3>{languageChosen}</h3>
+                    <h3>{languagesTranslated[posLang]}</h3>
                 </div>
             </body>
         </html>
