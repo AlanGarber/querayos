599c0b8e1ec5c620a459d6637e99013e91c6e29b
Alan Garber <alangarber1@gmail.com>
2023-03-28 12:15:57 -0300

    getting 3 random langs on a hook

--- app/page.tsx
+++ app/page.tsx
@@ -5,22 +5,29 @@ import React from "react";
 
 export default function HomePage() {
     const languages = wash.supported();
-    const [posLang,setPosLang]=useState()
     const [languageChosen, setlanguageChosen] = useState('')
     const [badWordLanguageChosen, setbadWordLanguageChosen] = useState()
     const [arrBadWords, setArrBadWords] = useState([])
-    const [languagesTranslated, setlanguagesTranslated] = useState([
+    const [languagesTranslated, setLanguagesTranslated] = useState([
         'belarusian', 'Bulgarian', 'Catalan', 'Czech', 'Welsh', 'Danish', 'German', 'Greek', 'English', 'Spanish', 'Estonian', 'Basque', 'Farsi', 'Finnish', 'French', 'Gaelic', 'Galician', 'Hindi', 'Croatian', 'Hungarian', 'Armenian', 'Indonesian', 'Icelandic', 'Italian', 'Japanese', 'Kannada', 'Korean', 'Latin', 'Lithuanian', 'Latvian', 'Macedonian', 'Malayalam', 'Mongolian', 'Marathi', 'Malay', 'Maltese', 'Burmese', 'Dutch', 'Polish', 'Portuguese', 'Romanian', 'Russian', 'Slovak', 'Slovenian', 'Albanian', 'Serbian', 'Swedish', 'Telugu', 'Thai', 'Turkish', 'Ukrainian', 'Uzbek', 'Vietnamese', 'Zulu'
     ])
+    const [languagesSelected, setLanguagesSelected] = useState([null, null, null])
 
 
     useEffect(() => {
         setlanguageChosen(languages[Math.floor(Math.random() * languages.length)])
+
+        for (let i = 0; i < 3; i++) {
+            let newArr = [...languagesSelected];
+            newArr[i] = languages[Math.floor(Math.random() * languages.length)];
+            setLanguagesSelected(newArr);
+            console.log(languagesSelected);
+            
+        }
     }, [])
 
     useEffect(() => {
         setArrBadWords(wash.words(languageChosen))
-        setPosLang(languages.indexOf(languageChosen))
     }, [languageChosen])
 
     useEffect(() => {
@@ -37,8 +44,10 @@ export default function HomePage() {
                 </div>
                 <div>
                     <h3>{badWordLanguageChosen}</h3>
-                    <h3>{languageChosen}</h3>
-                    <h3>{languagesTranslated[posLang]}</h3>
+                    <h3>{languagesTranslated[languages.indexOf(languageChosen)]}</h3>
+                    <h3>{languagesSelected[0]}</h3>
+                    <h3>{languagesSelected[1]}</h3>
+                    <h3>{languagesSelected[2]}</h3>
                 </div>
             </body>
         </html>
